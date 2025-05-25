4304de353f2f972afd032664dcd8eb03c2b1e318
Alejandro <@>
2025-05-25 16:11:10 +0200

    Grandes Avances | 25/05/2025

--- Script.sql
+++ Script.sql
@@ -3,7 +3,8 @@ CREATE TABLE Usuarios (
     nombre_usuario TEXT NOT NULL UNIQUE,
     contrasenia TEXT NOT NULL,
     correo TEXT NOT NULL UNIQUE,
-    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP
+    fecha_registro DATETIME,
+    es_admin BOOLEAN
 );
 
 
@@ -19,16 +20,16 @@ CREATE TABLE Progreso (
 CREATE TABLE Tesoros (
     id_tesoro INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT NOT NULL,
-    descripcion TEXT,
-    fecha_registro TEXT
+    descripcion TEXT
 );
 
 
-CREATE TABLE Tesoros_Obtenidos (
+CREATE TABLE TesorosObtenidos (
+    id_tesoro_obtenido INTEGER PRIMARY KEY AUTOINCREMENT,
     id_usuario INTEGER NOT NULL,
     id_tesoro INTEGER NOT NULL,
-    fecha_obtenido DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id_usuario, id_tesoro),
+    fecha_obtenido DATETIME,
+    UNIQUE (id_usuario, id_tesoro),
     FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario) ON DELETE CASCADE,
     FOREIGN KEY (id_tesoro) REFERENCES Tesoros(id_tesoro) ON DELETE CASCADE
 );
@@ -40,18 +41,26 @@ INSERT INTO Usuarios (nombre_usuario, contrasenia, correo) VALUES
 ('Jugador3', 'clave1234', 'jugador3@email.com');
 
 INSERT INTO Progreso (id_usuario, niveles_completados, puntuacion) VALUES
-(1, 5, 1500),
-(2, 3, 900),
-(3, 7, 2000);
+(1, 0, 0),
+(2, 0, 0),
+(3, 0, 0);
 
 
-INSERT INTO Tesoros (nombre, descripcion, fecha_registro) VALUES
-('Tesoro Dorado', 'Un antiguo tesoro de oro', '2025-04-04'),
-('Espada Legendaria', 'Una espada con poderes mágicos', '2025-04-04'),
-('Gema Mística', 'Una gema con energía desconocida', '2025-04-04');
+INSERT INTO Tesoros (nombre, descripcion) VALUES
+('Lapislázuli', 'El Lapislázuli es...'),
+('Ruby Rojo', 'El Rubí Rojo...'),
+('Diamante Poderoso', 'El Diamante Poderoso...'),
+('Brazalete del Poder', 'El Brazalete del Poder...'),
+('Anillo de las Sombras', 'En Anillo de las Sombras...'),
+('Anillo de la Valentía', 'El Anillo de la valentía...'),
+('Anillo de la Destrucción', 'El Anillo de la Destrucción...'),
+('Anillo de la Luz', 'El Anillo de la Luz...'),
+('Cobre Valioso', 'El Cobre Valioso...'),
+('Gema Mística', 'La Gema Mística...');
 
 
-INSERT INTO Tesoros_Obtenidos (id_usuario, id_tesoro, fecha_obtenido) VALUES
+
+INSERT INTO TesorosObtenidos (id_usuario, id_tesoro, fecha_obtenido) VALUES
 (1, 1, '2025-04-04 10:00:00'),
 (1, 2, '2025-04-04 11:00:00'),
 (2, 3, '2025-04-04 12:00:00');
